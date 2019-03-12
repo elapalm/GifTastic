8d365c9f88d8420d2fcaa02feb1ae1348d87a7c7
elapalm <45321080+elapalm@users.noreply.github.com>
2019-03-12 09:11:13 -0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,9 @@
 //The initial buttons on the page. Theme: Video Games.
 var items = ["Mario Bros.", "Halo", "Angry Birds", "Minecraft", "Sonic the Hedgehog", "Skull Monkies"];
 
-
- function showVideoGames() {
-    var videoGame = $("this").attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + items + "&api_key=O6oRTuA1g25yXcfN1ELtk7n3mB3fzOQR&limit=10"
-    
-    //making the buttons
-    function makeBUttons() {
-
-    $("#buttons").empty();
+ //making the buttons
+function makeButtons() {
+   $("#buttons").empty();
 
     for (var i = 0; i < items.length; i++); {
 
@@ -20,21 +14,26 @@ var items = ["Mario Bros.", "Halo", "Angry Birds", "Minecraft", "Sonic the Hedge
     $("buttons").append(newButton);
     }
 };
-  
-
-$("#add-game").on("click", function(event) {
-    event.preventDeffault();
-    var vgGame = $("game-input").val().trim();
-    vgGame.push(vgGame);
-    makeBUttons();
-}); 
-makeBUttons();
-
+ 
+ function showVideoGames() {
+    var videoGame = $("this").attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + items + "&api_key=O6oRTuA1g25yXcfN1ELtk7n3mB3fzOQR&limit=10"
     
     $.ajax({
         url: queryURL,
         method: "GET"
     })
+
+    $("#add-game").on("click", function(event) {
+      event.preventDeffault();
+      var vgGame = $("game-input").val().trim();
+      vgGame.push(vgGame);
+      makeButtons();
+}); 
+makeButtons();
+
+    
+    
     .then(function(response) {
         var gameRating = ('<div class="game-rating">');
         
