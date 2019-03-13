a81e61bf98bf60c1475245026d8b424b6ca5b111
emma lapalm <lillypadcrafts@gmail.com>
2019-03-12 17:19:49 -0700

    giving up. cant figure out why it isn't working

--- javascript/app.js
+++ javascript/app.js
@@ -1,58 +1,50 @@
 //The initial buttons on the page. Theme: Video Games.
 var items = ["Mario Bros.", "Halo", "Angry Birds", "Minecraft", "Sonic the Hedgehog", "Skull Monkies"];
 
- //making the buttons
+//making the buttons
 function makeButtons() {
-   $("#buttons").empty();
+    $("#buttons").empty();
 
     for (var i = 0; i < items.length; i++); {
 
-    var newButton = $("<button>").text(items[i]).attr("game-data", items[i]).addClass("vgButton");
-    $("#vgButtons").append(newButton)
+        var newButton = $("<button>").text(items[i]).attr("game-data", items[i]).addClass("vgButton");
+        $("#vgButtons").append(newButton)
+        console.log(newButton);
     }
 };
- 
- function showVideoGames() {
+/*
+// giphy search
+function showVideoGames() {
     $("#vgGifs").empty();
     var videoGame = $("this").attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + items + "&api_key=O6oRTuA1g25yXcfN1ELtk7n3mB3fzOQR&limit=10"
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
-   }).done(function(response) {
-     
-     var guestSearch = response.data;
-     
-     for (var i = 0; i < results.length; i==);
-          var gifsDiv = $("#<div>").addClass("gifs-div");
-          var rating = $("<h3>").html("Rating" + results[i].rating);
-          var still = results[i].images.fixed_height_still.url;
-          var animate = results[i].images.fixed_height.url;
-          var vgGif = $("<img>").addClass("vg-gif").attr({"src": still, "data-still": still, "data-animate": animate, "data-state": "still"});
-          gifsDiv.append(vgGif).append(rating);
-          $("#vgGifs").append(gifsDiv);
-    }
+    }).done(function (response) 
+};
+/*
+//adding new buttons aanad ratings for searches
+        var guestSearch = response.data;
+
+        for (var i = 0; i < results.length; i++);
+        var gifsDiv = $("#<div>").addClass("gifs-div");
+        var rating = $("<h3>").html("Rating" + results[i].rating);
+        var still = results[i].images.fixed_height_still.url;
+        var animate = results[i].images.fixed_height.url;
+        var vgGif = $("<img>").addClass("vg-gif").attr({ "src": still, "data-still": still, "data-animate": animate, "data-state": "still" });
+        gifsDiv.append(vgGif).append(rating);
+        $("#vgGifs").append(gifsDiv);
+    });
            
-  });
   
- }
+};
 
 $(document).on("click", "vgButton", showVideoGames);
-$(document).on("click", "vgButton", );
 
-
-
-
-    
-    
-    .then(function(response) {
-        var gameRating = ('<div class="game-rating">');
-        
-        var rating = response.rated;
-
-    });
-
-
-
- };
+var items = $("#guestSearch").val().trim()
+$("#guestSearch").val(""); (
+    guestSearch.push(guestSearch);
+    newButton();
+ 
